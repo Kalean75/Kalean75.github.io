9fdef6b541cf522d27473e6436d3e4fec442ae3c
Kalean75 <43306850+Kalean75@users.noreply.github.com>
2023-09-16 12:45:54 -0600

    Layout improvements

---
+++ Footer.php
@@ -0,0 +1,7 @@
+<?php 
+echo '<footer class="text-center">
+		<a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+			<span class="glyphicon glyphicon-chevron-up"></span>
+		</a>
+	</footer>'
+    ?>
