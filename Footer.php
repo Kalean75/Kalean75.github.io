c863617aec39b6ab442499a0e3e119e58a391e6a
Kalean75 <43306850+Kalean75@users.noreply.github.com>
2023-09-28 11:49:46 -0600

    Added copyright Information

--- Footer.php
+++ Footer.php
@@ -3,5 +3,6 @@ echo '<footer class="text-center">
 		<a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
 			<span class="glyphicon glyphicon-chevron-up"></span>
 		</a>
-	</footer>'
+	<p>© 2023. DW-Portfolio. Designed and Developed by Devin White</p>
+</footer>'
     ?>
