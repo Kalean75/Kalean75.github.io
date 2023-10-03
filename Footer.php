5fa8f94b70dcc1bff5cddedc1b22a434ac068d29
Devin White <WhiteDevint@Gmail.com>
2023-10-02 21:05:01 -0600

    updated PHP

--- Footer.php
+++ Footer.php
@@ -1,8 +1,8 @@
 <?php 
 echo '<footer class="text-center">
-		<a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
-			<span class="glyphicon glyphicon-chevron-up"></span>
-		</a>
-	<p>© 2023. DW-Portfolio. Designed and Developed by Devin White</p>
+<a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+	<span class="glyphicon glyphicon-chevron-up"></span>
+</a>
+<p>© 2023. DW-Portfolio. Designed and Developed by Devin White</p>
 </footer>'
     ?>
